29adc611b3779341ce36fb962349f89a8bda0603
graberyanis <yanisgraber006@gmail.com>
2024-03-23 20:01:02 +0100

    added melee buffering

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,7 @@ const SPEED = 400.0
 var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 var last_movement = input_direction.angle()
 var stab_ready = true
+var stab_released = true
 var bullet_ready = true
 var health = 5
 
@@ -41,13 +42,16 @@ func move():
 
 
 func stab():
-	if Input.is_action_pressed("ui_melee_attack") and stab_ready:
+	if Input.is_action_pressed("ui_melee_attack") and stab_ready and stab_released:
 		$Stab.rotation = last_movement - PI / 2
 		$Stab.visible = true
 		$Stab/StabCollision.disabled = false
+		stab_released = false
 		stab_ready = false
 		$StabDuration.start()
 		$StabCooldown.start()
+	if Input.is_action_just_released("ui_melee_attack"):
+		stab_released = true
 
 
 func shoot():
