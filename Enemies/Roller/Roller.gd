4e5eec16ed9be25596fb78deda2eeed7ae8dc599
graberyanis <yanisgraber006@gmail.com>
2024-04-04 11:29:32 +0200

    first commit

--- Enemies/Roller/Roller.gd
+++ Enemies/Roller/Roller.gd
@@ -1,14 +1,13 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const SPEED = 150.0
 var health = 2
-const friendship_distance = 300
+const friendship_distance = 500
 
 @onready var player = $/root/Game/Player
 
 
-func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
+func pathfind(direction):
 	var entities_in_vision = $Vision.get_overlapping_bodies()
 	var rollers_in_vision = Array ()
 	var rollers_nearby = Array ()
@@ -23,6 +22,12 @@ func _physics_process(delta):
 		elif roller in rollers_nearby:
 			direction -= 0.5 * global_position.direction_to(roller.global_position)
 	direction /= direction.length()
+	return direction
+
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	direction = pathfind(direction)
 	velocity = direction * SPEED
 	if global_position.direction_to(player.global_position)[0] > 0:
 		rotation += delta
