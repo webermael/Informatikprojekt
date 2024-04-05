124ad641e1960fb9a6744c465477bf4e4431c86c
graberyanis <yanisgraber006@gmail.com>
2024-04-05 13:05:54 +0200

    first commit

--- Player/weapons/Bullet/Bullet.gd
+++ Player/weapons/Bullet/Bullet.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
 const SPEED = 600
+const MAX_DISTANCE = 500
 var damage = 2
+var distance_traveled = 0
 
 
 func _process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * SPEED * delta
+	distance_traveled += direction.length() * SPEED * delta
+	if distance_traveled >= MAX_DISTANCE:
+		queue_free()
 
 
 func _on_body_entered(body):
