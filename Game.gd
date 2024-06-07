e342b50016937c93c5a8df75e645a4e9e58624c3
graberyanis <yanisgraber006@gmail.com>
2024-06-07 15:53:13 +0200

    cleaner music changes

--- Game.gd
+++ Game.gd
@@ -3,16 +3,20 @@ extends Node2D
 var in_cleared_room = true
 
 
-func change_song(delta, song, fade = 5):
-	if $AudioStreamPlayer.volume_db <= -40 and $AudioStreamPlayer.stream != song:
+func change_song(delta, song, fade = 10, instant = false):
+	if not instant:
+		if $AudioStreamPlayer.volume_db <= -40 and $AudioStreamPlayer.stream != song:
+			$AudioStreamPlayer.stream = song
+			$AudioStreamPlayer.play(0)
+		elif $AudioStreamPlayer.volume_db < 0 and $AudioStreamPlayer.stream == song:
+			$AudioStreamPlayer.volume_db += delta * (abs($AudioStreamPlayer.volume_db) * fade + 1)
+		elif $AudioStreamPlayer.volume_db > 0:
+			$AudioStreamPlayer.volume_db = 0
+		else:
+			$AudioStreamPlayer.volume_db -= delta * (abs($AudioStreamPlayer.volume_db) * fade + 1)
+	if instant and $AudioStreamPlayer.stream != song:
 		$AudioStreamPlayer.stream = song
 		$AudioStreamPlayer.play(0)
-	elif $AudioStreamPlayer.volume_db < 0 and $AudioStreamPlayer.stream == song:
-		$AudioStreamPlayer.volume_db += delta * (abs($AudioStreamPlayer.volume_db) * fade + 1)
-	elif $AudioStreamPlayer.volume_db > 0:
-		$AudioStreamPlayer.volume_db = 0
-	else:
-		$AudioStreamPlayer.volume_db -= delta * (abs($AudioStreamPlayer.volume_db) * fade + 1)
 
 
 func _process(delta):
@@ -21,7 +25,7 @@ func _process(delta):
 	elif in_cleared_room and $Player.health <= 2:
 		change_song(delta, load("res://Music/room_clear_low.mp3"))
 	elif not in_cleared_room:
-		change_song(delta, load("res://Music/Main.mp3"), 20)
+		change_song(delta, load("res://Music/Main.mp3"), 0, true)
 		#$AudioStreamPlayer.stream = load("res://Music/Main.mp3")
 		#$AudioStreamPlayer.play(0)
 		#$AudioStreamPlayer.volume_db = 24
