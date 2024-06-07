f81c7a326fb4c08367c8981945f07099bea68e14
graberyanis <yanisgraber006@gmail.com>
2024-06-08 01:34:25 +0200

    finished? music

--- Game.gd
+++ Game.gd
@@ -3,32 +3,44 @@ extends Node2D
 var in_cleared_room = true
 
 
-func change_song(delta, song, fade = 10, instant = false):
-	if not instant:
-		if $AudioStreamPlayer.volume_db <= -40 and $AudioStreamPlayer.stream != song:
-			$AudioStreamPlayer.stream = song
-			$AudioStreamPlayer.play(0)
-		elif $AudioStreamPlayer.volume_db < 0 and $AudioStreamPlayer.stream == song:
-			$AudioStreamPlayer.volume_db += delta * (abs($AudioStreamPlayer.volume_db) * fade + 1)
-		elif $AudioStreamPlayer.volume_db > 0:
-			$AudioStreamPlayer.volume_db = 0
+func change_song(delta, song, low_song, fade = 80, instant = false, stream_player = $Normal, not_stream_player = $Low):
+	if not instant and $Normal.stream != song and stream_player.volume_db <= 0:
+		print(stream_player.stream == song, stream_player.volume_db)
+		if stream_player.volume_db <= -40 and $Normal.stream != song:
+			$Normal.stream = song
+			$Normal.play(0)
+			$Low.stream = low_song
+			$Low.play(0)
+		elif stream_player.volume_db < 0 and stream_player.stream == song:
+			stream_player.volume_db += delta * fade
+		elif stream_player.volume_db > 0:
+			stream_player.volume_db = 0
 		else:
-			$AudioStreamPlayer.volume_db -= delta * (abs($AudioStreamPlayer.volume_db) * fade + 1)
-	if instant and $AudioStreamPlayer.stream != song:
-		$AudioStreamPlayer.stream = song
-		$AudioStreamPlayer.play(0)
+			print(stream_player.volume_db)
+			stream_player.volume_db -= delta * fade
+			print(stream_player.volume_db)
+	elif instant and $Normal.stream != song:
+		print(in_cleared_room, $Player.health, stream_player == $Low)
+		$Normal.stream = song
+		$Normal.play(0)
+		$Low.stream = low_song
+		$Low.play(0)
+		not_stream_player.volume_db = -80
+		stream_player.volume_db = 0
+	elif stream_player.volume_db != 0:
+		not_stream_player.volume_db = -80
+		stream_player.volume_db = 0
 
 
 func _process(delta):
 	if in_cleared_room and $Player.health > 2:
-		change_song(delta, load("res://Music/room_clear_normal.mp3"))
+		change_song(delta, load("res://Music/room_clear_normal.mp3"), load("res://Music/room_clear_low.mp3"))
 	elif in_cleared_room and $Player.health <= 2:
-		change_song(delta, load("res://Music/room_clear_low.mp3"))
-	elif not in_cleared_room:
-		change_song(delta, load("res://Music/Main.mp3"), 0, true)
-		#$AudioStreamPlayer.stream = load("res://Music/Main.mp3")
-		#$AudioStreamPlayer.play(0)
-		#$AudioStreamPlayer.volume_db = 24
+		change_song(delta, load("res://Music/room_clear_normal.mp3"), load("res://Music/room_clear_low.mp3"), 80, false, $Low, $Normal)
+	elif not in_cleared_room and $Player.health > 2:
+		change_song(delta, load("res://Music/Main.mp3"), load("res://Music/Main_Low.mp3"), 0, true)
+	elif not in_cleared_room and $Player.health <= 2:
+		change_song(delta, load("res://Music/Main.mp3"), load("res://Music/Main_Low.mp3"), 0, true, $Low, $Normal)
 
 
 func _on_dungeon_room_cleared():
