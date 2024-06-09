d2eca5e69cd3d017bcfe0137666b63910cb49b41
graberyanis <yanisgraber006@gmail.com>
2024-06-09 11:35:59 +0200

    cleaned up music code

--- Game.gd
+++ Game.gd
@@ -9,13 +9,13 @@ load("res://Music/Main_Low.mp3") : 0
 }
 
 
-func change_song(delta, song, low_song, fade = 80, instant = false, stream_player = $AudioStreamPlayer, not_stream_player = $Low):
+func change_song(delta, song, fade = 80, instant = false):
 	if not instant and $AudioStreamPlayer.stream != song and $AudioStreamPlayer.volume_db <= 0:
-		if stream_player.volume_db <= -40 and $AudioStreamPlayer.stream != song:
+		if $AudioStreamPlayer.volume_db <= -40 and $AudioStreamPlayer.stream != song:
 			song_positions[$AudioStreamPlayer.stream] = $AudioStreamPlayer.get_playback_position()
 			$AudioStreamPlayer.stream = song
 			$AudioStreamPlayer.play(song_positions[song])
-		elif $AudioStreamPlayer.volume_db < 0 and stream_player.stream == song:
+		elif $AudioStreamPlayer.volume_db < 0 and $AudioStreamPlayer.stream == song:
 			$AudioStreamPlayer.volume_db += delta * fade
 		else:
 			$AudioStreamPlayer.volume_db -= delta * fade
@@ -23,19 +23,19 @@ func change_song(delta, song, low_song, fade = 80, instant = false, stream_playe
 		song_positions[$AudioStreamPlayer.stream] = $AudioStreamPlayer.get_playback_position()
 		$AudioStreamPlayer.stream = song
 		$AudioStreamPlayer.play(song_positions[song])
-	elif stream_player.volume_db != 0:
-		stream_player.volume_db = 0
+	elif $AudioStreamPlayer.volume_db != 0:
+		$AudioStreamPlayer.volume_db = 0
 
 
 func _process(delta):
 	if in_cleared_room and $Player.health > 2:
-		change_song(delta, load("res://Music/room_clear_normal.mp3"), load("res://Music/room_clear_low.mp3"))
+		change_song(delta, load("res://Music/room_clear_normal.mp3"))
 	elif in_cleared_room and $Player.health <= 2:
-		change_song(delta, load("res://Music/room_clear_low.mp3"), load("res://Music/room_clear_low.mp3"), 80, false, $AudioStreamPlayer, $Normal)
+		change_song(delta, load("res://Music/room_clear_low.mp3"))
 	elif not in_cleared_room and $Player.health > 2:
-		change_song(delta, load("res://Music/Main.mp3"), load("res://Music/Main_Low.mp3"), 0, true)
+		change_song(delta, load("res://Music/Main.mp3"), 0, true)
 	elif not in_cleared_room and $Player.health <= 2:
-		change_song(delta, load("res://Music/Main_Low.mp3"), load("res://Music/Main_Low.mp3"), 0, true, $AudioStreamPlayer, $Normal)
+		change_song(delta, load("res://Music/Main_Low.mp3"), 0, true)
 
 
 func _on_dungeon_room_cleared():
